a0c4770640dc4d386d1f54e6a78fa6aa11492615
ViniciusZevarex <vinicius.zevarex2002@gmail.com>
2022-01-29 13:34:36 -0300

    Implementing Calculator

---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(secondNumber) && IsNumeric(firstNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+
+            return BadRequest("InvalidInput");
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, 
+                                System.Globalization.NumberStyles.Any, 
+                                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                                out number);
+
+            return isNumber;
+        }
+    }
+}
