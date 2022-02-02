5227f37941a5cb9244c69a1645d5b6ad22a6619f
ViniciusZevarex <vinicius.zevarex2002@gmail.com>
2022-02-02 07:35:50 -0300

    Http methods study

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -20,12 +20,78 @@ namespace RestWithASPNETUdemy.Controllers
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if (IsNumeric(secondNumber) && IsNumeric(firstNumber))
             {
-                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString());
+                var result = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+
+
+            return BadRequest("InvalidInput");
+        }
+
+
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(secondNumber) && IsNumeric(firstNumber))
+            {
+                var result = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+
+
+            return BadRequest("InvalidInput");
+        }
+
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(secondNumber) && IsNumeric(firstNumber))
+            {
+                var result = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+
+
+            return BadRequest("InvalidInput");
+        }
+
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(secondNumber) && IsNumeric(firstNumber))
+            {
+                var result = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+
+
+            return BadRequest("InvalidInput");
+        }
+
+        [HttpGet("square-root/{firstNumber}/{secondNumber}")]
+        public IActionResult SquareRoot(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var result = Math.Sqrt((double)ConvertToDecimal(firstNumber));
+                return Ok(result.ToString());
+            }
+
+
+            return BadRequest("InvalidInput");
+        }
+
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public IActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(secondNumber) && IsNumeric(firstNumber))
+            {
+                var result = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber))/2;
+                return Ok(result.ToString());
             }
 
 
---
+++ 03_RestWithASPNETUdemy_HttpMethods/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonService.cs
@@ -0,0 +1,112 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonService : IPersonService
+    {
+
+
+        private readonly MySQLContext _dbContext;
+
+        public PersonService(MySQLContext mySQLDbContext)
+        {
+            _dbContext = mySQLDbContext;
+        }
+
+
+
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _dbContext.Persons.Add(person);
+                _dbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+            return person;
+        }
+
+
+
+
+        public void Delete(long id)
+        {
+            var result = _dbContext.Persons.SingleOrDefault(p => p.Id == id);
+
+            if (result != null)
+            {
+                try
+                {
+                    _dbContext.Persons.Remove(result);
+                    _dbContext.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+
+        }
+
+
+
+        public List<Person> FindAll()
+        {
+            var persons = _dbContext.Persons.ToList();
+            return persons;
+        }
+
+
+
+
+        public Person FindById(long id)
+        {
+            return _dbContext.Persons.SingleOrDefault(p => p.Id == id);
+        }
+
+
+
+
+        public Person Update(Person person)
+        {
+            if (Exists(person.Id)) return new Person();
+
+
+            var result = _dbContext.Persons.SingleOrDefault(p => p.Id == person.Id);
+
+            if (result != null)
+            {
+                try
+                {
+                    _dbContext.Entry(result).CurrentValues.SetValues(result);
+                    _dbContext.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+
+            return person;
+        }
+
+
+
+
+        private bool Exists(long id)
+        {
+            return _dbContext.Persons.Any(p => p.Id == id);
+        }
+    }
+}
