b7d4718d03b3def435ec032760f3f06b360b486d
ViniciusZevarex <vinicius.zevarex2002@gmail.com>
2022-02-02 07:46:29 -0300

    API versioning

---
+++ 04_RestWithASPNETUdemy_EndpointVersioning/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+
+        Person Create(Person person);
+        Person FindById(long id);
+        Person Update(Person person);
+        List<Person> FindAll();
+        void Delete(long id);
+
+    }
+}
